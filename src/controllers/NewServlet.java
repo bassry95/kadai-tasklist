6024a731be0a4fba5f8b14f37cc7d9f264d454b4
bassry <bassry95@gmail.com>
2018-10-25 18:53:30 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,14 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
  */
-@WebServlet(name = "new", urlPatterns = { "/new" })
+@WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,32 +26,15 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    em.getTransaction().begin();
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("task", new Task());
 
-	    Task t = new Task();
-
-	    String title = "買い物";
-	    t.setTitle(title);
-
-	    String content = "歯磨き";
-	    t.setContent(content);
-
-	    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-	    t.setCreated_at(currentTime);
-	    t.setUpdated_at(currentTime);
-
-	    em.persist(t);
-	    em.getTransaction().commit();
-
-	    response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-	    em.close();
-	}
-
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
+    }
 
 }
